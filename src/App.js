d22e1c9a74dc1b34aa43d6e528b9f70681629c4a
Noel Moon <nmoon778@gmail.com>
2019-12-17 19:20:55 -0500

    added color boxes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Palette from './Palette.js';
+import seedColors from './seedColors.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Palette {...seedColors[4]}/>
     </div>
   );
 }
