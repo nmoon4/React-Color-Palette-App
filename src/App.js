5865683e75cc4399058c991c2256ba2a11008e92
Noel Moon <nmoon778@gmail.com>
2019-12-30 16:04:04 -0500

    added color helper to generate shades of colors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 // import './App.css';
 import Palette from './Palette.js';
 import seedColors from './seedColors.js';
+import {generatePalette} from './colorHelpers';
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]}/>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+
+    return (
+      <div>
+        <Palette {...seedColors[4]}/>
+      </div>
+    )
+  }
 }
-
-export default App;
