da762352565f2cbaaf1240b8eab8a362594abf01
Noel Moon <nmoon778@gmail.com>
2020-01-06 19:06:28 -0500

    added format selector and installed material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import "rc-slider/assets/index.css";
-import './Navbar.css'
-
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
+import "rc-slider/assets/index.css";
+import './Navbar.css';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {format: "hex"};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({format: e.target.value});
+    this.props.handleChange(e.target.value);
+  }
+
   render() {
     const {level, changeLevel} = this.props;
+    const {format} = this.state;
 
     return (
       <header className="Navbar">
@@ -28,8 +41,13 @@ export default class Navbar extends Component {
           </div>
         </div>
 
-        
-
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGB - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
 
       </header>
     )
